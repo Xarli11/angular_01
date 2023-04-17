d6c9d122429032d023cbbdeb95f539b1ff7bbbab
Xarli11 <carlosgt2001@icloud.com>
2023-04-17 13:01:55 +0200

    Añadido el componente de las union

---
+++ fundamentosTS/src/app/union/union.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-union',
+  templateUrl: './union.component.html',
+  styleUrls: ['./union.component.css']
+})
+export class UnionComponent {
+  //Union - Unión de tipos
+  string_number: (string | number)[] = ["Hola", 1, "Adios", 2, "Buenos días", 3];
+  ejemploNumber: number = 1|2|3|4|5|6|7|8|9|10;
+  ejemploString: "Hola"|"Adios"|"Buenos días"="Hola"; //Si no se inicializa, da error
+  diaCumple:number=1;
+  yearCumple:number=1990;
+  mes: meses= meses.junio;
+  //Constructor
+  constructor(){
+    this.mostrar();
+    console.log(this.ejemploNumber);
+    console.log(this.ejemploString);
+  }
+
+  //Metodos
+  mostrar(){
+    let resultado="";
+    for(let i=0;i<this.string_number.length;i++){
+      resultado=resultado+this.string_number[i]+" ";
+    }
+    console.log(resultado);
+  }
+
+  //Metodos para añadir
+  addString(){
+    this.string_number.push(this.ejemploString);
+    console.log("Añadido con exito: "+this.ejemploString);
+    this.mostrar();
+  }
+  addNumber(){
+    this.string_number.push(this.ejemploNumber);
+    console.log("Añadido con exito: "+this.ejemploNumber);
+    this.mostrar();
+  }
+  mostrarCumple(){
+    return this.diaCumple+" de "+this.mes+" de "+this.yearCumple;
+  }
+}
+enum meses{
+  enero="Enero",
+  febrero="Febrero",
+  marzo="Marzo",
+  abril="Abril",
+  mayo="Mayo",
+  junio="Junio",
+  julio="Julio",
+  agosto="Agosto",
+  septiembre="Septiembre",
+  octubre="Octubre",
+  noviembre="Noviembre",
+  diciembre="Diciembre"
+}
+
