1248e7323456cf0b72a4af4dd6a1bc95819b2bc8
Xarli11 <carlosgt2001@icloud.com>
2023-04-19 12:55:59 +0200

    Algunos comentarios añadidos

--- fundamentosRouting/src/app/app-routing.module.ts
+++ fundamentosRouting/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// Importar los componentes que se van a enlazar a las rutas
 import { CRUDComponent } from './crud/crud.component';
 import { InicioComponent } from './inicio/inicio.component';
 import {QuienessomosComponent} from './quienessomos/quienessomos.component';
 
 const routes: Routes = [
+  // Asignar un componente a una ruta
   { path: 'InicioComponent', component:   InicioComponent },
   { path: 'QuienessomosComponent', component: QuienessomosComponent },
   { path: 'CRUDComponent', component: CRUDComponent },
