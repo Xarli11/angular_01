e3697569310344e60dc1d9e04ee0fa7d0feceeae
Xarli11 <carlosgt2001@icloud.com>
2023-04-26 09:58:00 +0200

    Modificacion de estetica

--- Routing3/src/app/curso-alta/curso-alta.component.ts
+++ Routing3/src/app/curso-alta/curso-alta.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Curso, Nivel } from '../_modelo/curso';
 import { CursosService } from '../cursos.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -10,27 +10,34 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./curso-alta.component.css']
 })
 export class CursoAltaComponent {
- niveles=Object.values(Nivel);
- curso:Curso=new Curso(0,"",0, Nivel.INICIACION);
+  niveles = Object.values(Nivel);
+  curso: Curso = new Curso(0, "", 0, Nivel.INICIACION);
   cursos: Curso[] = [];
 
   constructor(private cursoService: CursosService, private route: ActivatedRoute, private router: Router) {
     this.cursos = this.cursoService.cursos;
-    this.curso.id=route.snapshot.params["id"];
+    this.curso.id = route.snapshot.params["id"];
   }
-  modificarCurso(curso: Curso){
+  ngOnInit(): void {
+    this.route.params.subscribe(data => {
+      this.curso.id = data['id'];
+    });
+  }
+
+  modificarCurso(curso: Curso) {
     this.cursoService.modificarCurso(curso.id, curso.nombre, curso.duracion, curso.nivel);
     this.router.navigate(['/']);
-    }
-  altaCurso(){
+  }
+
+  altaCurso() {
     console.log("altaCurso");
-    this.curso.id=(this.cursos[this.cursos.length-1].id)+1;
+    this.curso.id = (this.cursos[this.cursos.length - 1].id) + 1;
     this.cursos.push(this.curso)
   }
-  modifAltaCurso(){
-    if(this.curso.id==undefined){
+  modifAltaCurso() {
+    if (this.curso.id == undefined) {
       this.altaCurso();
-    }else{
+    } else {
       this.modificarCurso(this.curso);
     }
     this.router.navigate(['/']);
